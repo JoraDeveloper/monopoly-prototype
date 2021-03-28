0712b5720510aca083f6c37e7a24184e7c0032b1
Test <viktorpilevins@gmail.com>
2021-03-28 16:39:08 +0300

    setup project

---
+++ src/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {render} from 'react-dom';
+import './style.scss';
+
+const App:React.FC = () => {
+    return (
+        <div className='container'>
+        </div>
+    )
+}
+
+
+render(<App />, document.getElementById('root'))
